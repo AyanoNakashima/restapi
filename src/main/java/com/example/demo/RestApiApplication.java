cfa327d4c422f72aa923997b4046f53bf23e19a1
AyanoNakashima <ayano.nakashima@lifebox.co.jp>
2021-03-17 22:08:05 +0900

    完成

--- src/main/java/com/example/demo/RestApiApplication.java
+++ src/main/java/com/example/demo/RestApiApplication.java
@@ -1,9 +1,14 @@
-package com.example;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan("com.example")
+@EntityScan("com.example")
+@EnableJpaRepositories("com.example")
 @SpringBootApplication
 public class RestApiApplication {
 
